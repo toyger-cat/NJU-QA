e3d5fc93a08ec935d41c411aac63ad3f55e5c600
toyger-cat <toyger-cat@outlook.com>
2024-02-12 00:01:07 +0800

    refresh

--- llm/qwen14_llm.py
+++ llm/qwen14_llm.py
@@ -9,6 +9,29 @@
 
 from dotenv import find_dotenv, load_dotenv
 import os
+from http import HTTPStatus
+import dashscope
 
 _ = load_dotenv(find_dotenv())
-wenxin_api_key = os.environ["DASHSCOPE_API_KEY"]
+DASHSCOPE_API_KEY = os.environ["DASHSCOPE_API_KEY"]
+
+def call_with_messages():
+    messages = [{'role': 'system', 'content': 'You are a helpful assistant.'},
+                {'role': 'user', 'content': '如何做炒西红柿鸡蛋？'}]
+
+    response = dashscope.Generation.call(
+        dashscope.Generation.Models.qwen_turbo,
+        messages=messages,
+        result_format='message',  # set the result to be "message" format.
+    )
+    if response.status_code == HTTPStatus.OK:
+        print(response)
+    else:
+        print('Request id: %s, Status code: %s, error code: %s, error message: %s' % (
+            response.request_id, response.status_code,
+            response.code, response.message
+        ))
+
+
+# if __name__ == '__main__':
+#     call_with_messages()
